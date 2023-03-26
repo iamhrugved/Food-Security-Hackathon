af1b288092c13d81ea58db841914334d41931865
Jui <thombrej@gene.com>
2023-03-26 14:13:33 -0700

    Sensor data

--- backend/index.js
+++ backend/index.js
@@ -27,6 +27,7 @@ app.use(require("./routes/debtservices"));
 app.use(require("./routes/agriservices"));
 app.use(require("./routes/importservices"));
 app.use(require("./routes/yieldservices"));
+app.use(require("./routes/sensorservices"));
 
 const PORT = process.env.PORT || 5000;
 //Server code will be running on port 5000
