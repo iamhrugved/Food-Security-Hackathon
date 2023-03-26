468522f6b7a6c8c3b4955fb4901ed733bc2730ad
Hrugved Pawar <hrugved.pawar@sjsu.edu>
2023-03-26 14:20:59 -0700

    server changes

--- backend/index.js
+++ backend/index.js
@@ -29,6 +29,6 @@ app.use(require("./routes/importservices"));
 app.use(require("./routes/yieldservices"));
 app.use(require("./routes/sensorservices"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 //Server code will be running on port 5000
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
