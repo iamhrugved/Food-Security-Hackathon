3c2e0dd231dad3f101c395e0d7c13d2ada666d80
Hrugved Pawar <hrugved.pawar@sjsu.edu>
2023-03-26 15:34:16 -0700

    final commit

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ app.use(express.json({ extended: false }));
 
 app.use(function (req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    // res.setHeader("Access-Control-Allow-Origin", "https://visionary-quokka-899780.netlify.app");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader(
       "Access-Control-Allow-Methods",
